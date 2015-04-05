54204e173dc4d781586ddf339ca0580276740e60
edendramis <evangeliad@gmail.com>
2015-04-04 18:48:57 -0700

    adding linters, fixing lint errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,31 @@
 'use strict';
 
-var path      = require('path');
+var path = require('path');
 
-var gulp      = require('gulp');
-var less      = require('gulp-less');
-var rename    = require('gulp-rename');
+var gulp = require('gulp');
+var less = require('gulp-less');
+var rename = require('gulp-rename');
 var minifyCss = require('gulp-minify-css');
-var prefix    = require('gulp-autoprefixer');
+var prefix = require('gulp-autoprefixer');
 
 var BASE_DIR = path.join(__dirname, 'src', 'main', 'org', 'freemarker', 'docgen');
-var OUT_DIR  = path.join(BASE_DIR, 'statics');
-var TEMP_OUT = '/Users/evangeliadendramis/Documents/workspace/freemarker/build/manual/docgen-resources'
+var OUT_DIR = path.join(BASE_DIR, 'statics');
+var TEMP_OUT = '/Users/evangeliadendramis/Documents/workspace/freemarker/build/manual/docgen-resources';
 
 gulp.task('styles', function() {
   gulp.src(path.join(BASE_DIR, 'less', 'styles.less'))
-    .pipe( less( { paths: path.join(__dirname, 'node_modules') } ) )
+    .pipe(less({ paths: path.join(__dirname, 'node_modules') }))
 
     // rename and prefix
-    .pipe( rename( { basename: "docgen" } ) )
-    .pipe( prefix( { cascade: false } ) )
-    .pipe( gulp.dest( OUT_DIR ) )
-    .pipe( gulp.dest( TEMP_OUT ) )
+    .pipe(rename({ basename: 'docgen' }))
+    .pipe(prefix({ cascade: false }))
+    .pipe(gulp.dest(OUT_DIR))
+    .pipe(gulp.dest(TEMP_OUT))
 
     // minify
-    .pipe( rename( { suffix: ".min" } ) )
-    .pipe( minifyCss() )
-    .pipe( gulp.dest( OUT_DIR ) )
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifyCss())
+    .pipe(gulp.dest(OUT_DIR));
 });
 
 gulp.task('default', ['styles']);
