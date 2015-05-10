0b6c2e993ecfc0ecbe10d191c17abdd1621f1464
edendramis <evangeliad@gmail.com>
2015-05-09 23:51:02 -0700

    separating toc into own file, fixing logic for page menu. this resolves freemarker/docgen#17

---
+++ src/main/org/freemarker/docgen/statics/page-menu.js
@@ -0,0 +1,55 @@
+(function() {
+
+  // remove highlight class so animation can be repeated on same node again
+  function unHighlightNode(nodeId) {
+    var node = document.getElementById(nodeId);
+
+    if (node) {
+      node.classList.remove('active');
+    }
+  }
+
+  function highlightNode(nodeId) {
+    var node = document.getElementById(nodeId);
+
+    if (node) {
+      node.classList.add('active');
+
+      window.setTimeout(function() {
+        unHighlightNode(nodeId);
+      }, 500);
+    }
+  }
+
+  function onPageMenuClick(e) {
+    var node = e.target;
+
+    if (node.classList.contains('page-menu-link')) {
+      highlightNode(node.getAttribute('data-menu-target'));
+    }
+  }
+
+  function getNodeIdFromHash() {
+    if (window.location.hash) {
+      return window.location.hash.substring(1);
+    } else {
+      return '';
+    }
+  }
+
+  function onDocReady() {
+    var nodeId = getNodeIdFromHash();
+
+    if (nodeId !== '') {
+      highlightNode(nodeId);
+    }
+  }
+
+  function init() {
+    document.addEventListener('click', onPageMenuClick);
+    document.addEventListener('DOMContentLoaded', onDocReady);
+  }
+
+  init();
+
+})();
